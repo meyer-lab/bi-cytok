7c3ea6b854ada8ca2814cf9e3754799d506b941c
Peter Emmel <petercemmel@gmail.com>
2021-10-14 11:38:03 -0700

    abundances calc

--- ckine/figures/figure5.py
+++ ckine/figures/figure5.py
@@ -13,6 +13,8 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import RidgeClassifierCV
 from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import SVC
+from sklearn.preprocessing import LabelBinarizer
 
 path_here = dirname(dirname(__file__))
 
@@ -22,29 +24,118 @@ def makeFigure():
     cellTarget = "Treg"
     epitopesDF = pd.DataFrame(columns={"Classifier", "Epitope"})
 
-    posCorrs1, negCorrs = CITE_RIDGE(ax[4], cellTarget)
-    for x in posCorrs1:
-        epitopesDF = epitopesDF.append(pd.DataFrame({"Classifier": 'CITE_RIDGE', "Epitope": [x]}))
-    possCorrs2 = distMetricScatt(ax[6:8], cellTarget, 10, weight=False)
-    for x in possCorrs2:
-        epitopesDF = epitopesDF.append(pd.DataFrame({"Classifier": 'distMetricF', "Epitope": [x]}))
-    possCorrs3 = distMetricScatt(ax[8:10], cellTarget, 10, weight=True)
-    for x in possCorrs3:
-        epitopesDF = epitopesDF.append(pd.DataFrame({"Classifier": 'distMetricT', "Epitope": [x]}))
+    # possCorrs0 = CITE_SVM(ax[0], cellTarget)
+    # for x in possCorrs0:
+    #     epitopesDF = epitopesDF.append(pd.DataFrame({"Classifier": 'CITE_SVM', "Epitope": [x]}))
+    # print(epitopesDF)
+    # posCorrs1, negCorrs = CITE_RIDGE(ax[4], cellTarget)
+    # for x in posCorrs1:
+    #     epitopesDF = epitopesDF.append(pd.DataFrame({"Classifier": 'CITE_RIDGE', "Epitope": [x]}))
+    # possCorrs2 = distMetricScatt(ax[6:8], cellTarget, 10, weight=False)
+    # for x in possCorrs2:
+    #     epitopesDF = epitopesDF.append(pd.DataFrame({"Classifier": 'distMetricF', "Epitope": [x]}))
+    # possCorrs3 = distMetricScatt(ax[8:10], cellTarget, 10, weight=True)
+    # for x in possCorrs3:
+    #     epitopesDF = epitopesDF.append(pd.DataFrame({"Classifier": 'distMetricT', "Epitope": [x]}))
+    # print(epitopesDF)
+    
+    # epitopesDF.to_csv(join(path_here, "data/epitopeList.csv"), index=False)
+
+    # Comment out running stuff
+    # Import existing data frame
+    # Import cite data
+    epitopesDF = pd.read_csv(join(path_here, "data/epitopeList.csv"))
+
+    CITE_DF = importCITE()
+    #print(CITE_DF.CellType1.unique())
+    #print(CITE_DF.CellType2.unique())
+    #print(CITE_DF.CellType3.unique())
+
+    # Get conv factors, average them
+    convFact = convFactCalc(ax[0])
+    print(convFact)
+    meanConv = convFact.Weight.mean()
+    print(meanConv)
+
+    # Import cite data into dataframe
+    tregDF = CITE_DF.loc[CITE_DF["CellType2"] == 'Treg'].sample()
+    nkDF = CITE_DF.loc[CITE_DF["CellType1"] == 'NK'].sample()
+    
+    thelperDF = CITE_DF.loc[CITE_DF["CellType2"]=='CD4 Naive']
+    thelperDF = thelperDF.append(CITE_DF.loc[CITE_DF["CellType2"]=='CD4 CTL'])
+    thelperDF = thelperDF.append(CITE_DF.loc[CITE_DF["CellType2"]=='CD4 TCM'])
+    thelperDF = thelperDF.append(CITE_DF.loc[CITE_DF["CellType2"]=='CD4 TEM'])
+    thelperDF = thelperDF.sample()
+    
+    cd8DF = CITE_DF.loc[CITE_DF["CellType2"]=='CD8 Naive']
+    cd8DF = cd8DF.append(CITE_DF.loc[CITE_DF["CellType2"]=='CD8 TCM'])
+    cd8DF = cd8DF.append(CITE_DF.loc[CITE_DF["CellType2"]=='CD8 TEM'])
+    cd8DF = cd8DF.sample()
+
+
+    
+    treg_abundances = []
+    thelper_abundances = []
+    nk_abundances = []
+    cd8_ abundances []
+    for e in epitopesDF.Epitope:
+        #print(e)
+        citeVal = tregDF[e].item() # Add 3 special cases
+        abundance = citeVal*meanConv
+        abundances.append(abundance)
+    
+    # Import actual abundance into dadaframe by multiplying by average abundance (CD25, CD122, CD132 exceptions)
+    epitopesDF['Abundance'] = abundances
     print(epitopesDF)
-    #do for Cite_SVM
+    
+    # Feed actual abundance into modeling
 
-    #put these three in data frame, get abundance and affinity data
-
-    #use minSelect function 
+    # Use minSelect function 
         #Feed into bispec binding model
     
-    #optimize using minSelect
+    # Optimize using minSelect
 
 
 
     return f
 
+def CITE_SVM(ax, targCell, numFactors=10, sampleFrac=0.2):
+    """Fits a ridge classifier to the CITE data and plots those most highly correlated with T reg"""
+    SVMmod = SVC()
+    SVC_DF = importCITE()
+    cellToI = SVC_DF.CellType2.unique()
+    SVC_DF = SVC_DF.loc[(SVC_DF["CellType2"].isin(cellToI)), :]
+    SVC_DF = SVC_DF.sample(frac=sampleFrac, random_state=1)
+    cellTypeCol = SVC_DF.CellType2.values
+    SVC_DF = SVC_DF.loc[:, ((SVC_DF.columns != 'CellType1') & (SVC_DF.columns != 'CellType2') & (SVC_DF.columns != 'CellType3') & (SVC_DF.columns != 'Cell'))]
+    factors = SVC_DF.columns
+    X = SVC_DF.values
+    X = StandardScaler().fit_transform(X)
+    CD25col = X[:, np.where(factors == "CD25")].reshape(-1, 1)
+
+    enc = LabelBinarizer()
+    y = enc.fit_transform(cellTypeCol)
+    TregY = y[:, np.where(enc.classes_ == targCell)].ravel()
+
+    AccDF = pd.DataFrame(columns=["Markers", "Accuracy"])
+    baselineAcc = SVMmod.fit(CD25col, TregY).score(CD25col, TregY)
+    #print(baselineAcc)
+    #print(np.where((factors == "CD25")))
+    for marker in factors:
+        SVMmod = SVC()
+        #print(marker)
+        markerCol = X[:, np.where(factors == marker)]
+        CD25MarkX = np.hstack((CD25col, markerCol.reshape(-1, 1)))
+        markAcc = SVMmod.fit(CD25MarkX, TregY).score(CD25MarkX, TregY)
+        #print(markAcc)
+        AccDF = AccDF.append(pd.DataFrame({"Markers": [marker], "Accuracy": [markAcc]}))
+
+    AccDF = AccDF.sort_values(by="Accuracy")
+    markers = copy(AccDF.tail(numFactors).Markers.values) #Here
+    AccDF.Markers = "CD25 + " + AccDF.Markers
+
+    print(markers)
+    return markers
 
 def CITE_RIDGE(ax, targCell, numFactors=10):
     """Fits a ridge classifier to the CITE data and plots those most highly correlated with T reg"""
@@ -152,6 +243,54 @@ def cytBindingModel_bispecOpt(recX, recXaff, cellType, x=False):
         else:
             output *= convDict.loc[(convDict.Date == date) & (convDict.Cell == cellType)].Scale.values
     return output
+
+cellDict = {"CD4 Naive": "Thelper",
+            "CD4 CTL": "Thelper",
+            "CD4 TCM": "Thelper",
+            "CD4 TEM": "Thelper",
+            "NK": "NK",
+            "CD8 Naive": "CD8",
+            "CD8 TCM": "CD8",
+            "CD8 TEM": "CD8",
+            "Treg": "Treg"}
+
+
+markDict = {"CD25": "IL2Ra",
+            "CD122": "IL2Rb",
+            "CD127": "IL7Ra",
+            "CD132": "gc"}
+
+def convFactCalc(ax):
+    """Fits a ridge classifier to the CITE data and plots those most highly correlated with T reg"""
+    CITE_DF = importCITE()
+    cellToI = ["CD4 TCM", "CD8 Naive", "NK", "CD8 TEM", "CD4 Naive", "CD4 CTL", "CD8 TCM", "Treg", "CD4 TEM"]
+    markers = ["CD122", "CD127", "CD25"]
+    markerDF = pd.DataFrame(columns=["Marker", "Cell Type", "Amount", "Number"])
+    for marker in markers:
+        for cell in cellToI:
+            cellTDF = CITE_DF.loc[CITE_DF["CellType2"] == cell][marker]
+            markerDF = markerDF.append(pd.DataFrame({"Marker": [marker], "Cell Type": cell, "Amount": cellTDF.mean(), "Number": cellTDF.size}))
+
+    markerDF = markerDF.replace({"Marker": markDict, "Cell Type": cellDict})
+    markerDFw = pd.DataFrame(columns=["Marker", "Cell Type", "Average"])
+    for marker in markerDF.Marker.unique():
+        for cell in markerDF["Cell Type"].unique():
+            subDF = markerDF.loc[(markerDF["Cell Type"] == cell) & (markerDF["Marker"] == marker)]
+            wAvg = np.sum(subDF.Amount.values * subDF.Number.values) / np.sum(subDF.Number.values)
+            markerDFw = markerDFw.append(pd.DataFrame({"Marker": [marker], "Cell Type": cell, "Average": wAvg}))
+
+    recDF = importReceptors()
+    weightDF = pd.DataFrame(columns=["Receptor", "Weight"])
+
+    for rec in markerDFw.Marker.unique():
+        CITEval = np.array([])
+        Quantval = np.array([])
+        for cell in markerDF["Cell Type"].unique():
+            CITEval = np.concatenate((CITEval, markerDFw.loc[(markerDFw["Cell Type"] == cell) & (markerDFw["Marker"] == rec)].Average.values))
+            Quantval = np.concatenate((Quantval, recDF.loc[(recDF["Cell Type"] == cell) & (recDF["Receptor"] == rec)].Mean.values))
+        weightDF = weightDF.append(pd.DataFrame({"Receptor": [rec], "Weight": np.linalg.lstsq(np.reshape(CITEval, (-1, 1)), Quantval, rcond=None)[0]}))
+
+    return weightDF
 
 def minSelecFunc(x, targCell, offTCells):
     """Provides the function to be minimized to get optimal selectivity"""
