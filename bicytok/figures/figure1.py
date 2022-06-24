6fe19dcc598ea95e1eeea227f9814308d2e6ae1c
EmilyTLin <powerless300@gmail.com>
2022-06-24 13:52:22 -0700

    Generate output array of receptors bound per dose per valency for test antibody.

--- bicytok/figures/figure1.py
+++ bicytok/figures/figure1.py
@@ -7,6 +7,7 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 
+from valentbind import polyc
 
 path_here = dirname(dirname(__file__))
 
@@ -15,5 +16,13 @@ def makeFigure():
     """Get a list of the axis objects and create a figure"""
     ax, f = getSetup((9, 12), (1, 1))
 
+    doseVec = np.logspace(-12, -6, num=100)
+    vals = [1, 2, 3, 4]
+
+    output = np.zeros(doseVec.size)
+
+    for val in vals:
+        for i, dose in enumerate(doseVec):
+            output[i] = polyc(dose, 1e-12, [10000], [[val]], [1.0], np.array([[1e9]]))[0][1]
 
     return f
