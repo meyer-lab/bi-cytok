4a27d6322308a3c1f52e96f12e19f3dc4018ec0a
EmilyTLin <powerless300@gmail.com>
2023-01-23 11:42:11 -0800

    Generate bar plot of IL2Rb bound per cell type

---
+++ bicytok/figures/figure11.py
@@ -0,0 +1,32 @@
+"""
+This creates Figure 11, used to plot amount of IL2Rb bound to each cell type.
+"""
+from os.path import dirname, join
+from .common import getSetup
+import pandas as pd
+import seaborn as sns
+import numpy as np
+
+from ..selectivityFuncs import get_cell_bindings, getSampleAbundances
+
+
+path_here = dirname(dirname(__file__))
+
+
+def makeFigure():
+    """Get a list of the axis objects and create a figure"""
+    ax, f = getSetup((9, 12), (1, 1))
+
+    cells = ['CD8 Naive', 'NK', 'CD8 TEM', 'CD4 Naive', 'CD4 CTL', 'CD8 TCM',
+    'Treg', 'CD4 TEM', 'NK Proliferating', 'NK_CD56bright']
+
+    epitopesList = pd.read_csv(join(path_here, "data/epitopeList.csv"))
+    epitopes = list(epitopesList['Epitope'].unique())
+    epitopesDF = getSampleAbundances(epitopes, cells)
+
+    bindings = get_cell_bindings([8.5, 6.0, 8.5], cells, epitopesDF, epitopes[17], 0.1, False)
+
+    print(bindings)
+    sns.barplot(data=bindings, x='Cell Type', y='IL2Rb Bound')
+
+    return f
