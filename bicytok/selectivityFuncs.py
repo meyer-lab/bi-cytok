eba4ef23d28571825fbdda30edba133df0f6e7c8
EmilyTLin <powerless300@gmail.com>
2023-01-23 04:17:30 -0800

    Make get_cell_bindings function to output IL2Rb bound to each cell type

--- bicytok/selectivityFuncs.py
+++ bicytok/selectivityFuncs.py
@@ -335,4 +335,32 @@ def get_rec_vecs(df: pd.DataFrame, targCell: string, offTCells: list, epitope: s
 
     return np.array([cd25CountTarg, cd122CountTarg, epCountvecTarg]), np.array([cd25CountOffT, cd122CountOffT, epCountvecOffT])
 
+def get_cell_bindings(affs: float, cells: np.array, selectedDF: pd.DataFrame, epitope: string, dose: float, IL2Ra: bool):
+    df = pd.DataFrame(columns=['Cell Type', 'IL2Rb Bound'])
+
+    for cell in cells:
+        cd25DF = selectedDF.loc[(selectedDF.Epitope == 'CD25')]
+        cd122DF = selectedDF.loc[(selectedDF.Epitope == 'CD122')]
+        epitopeDF = selectedDF.loc[(selectedDF.Epitope == epitope)]
+
+        cd25CountTarg = np.zeros((epitopeDF[cell].item().size))
+        cd122CountTarg = np.zeros((epitopeDF[cell].item().size))
+        epCountvecTarg = np.zeros((epitopeDF[cell].item().size))
+
+        for i, epCount in enumerate(epitopeDF[cell].item()):
+            cd25CountTarg[i] = cd25DF[cell].item()[i]
+            cd122CountTarg[i] = cd122DF[cell].item()[i]
+            epCountvecTarg[i] = epCount
+        
+        recs = np.array([cd25CountTarg, cd122CountTarg, epCountvecTarg])
+
+        IL2RbBound = np.sum(bispecOpt_Vec(recs[0, :], recs[1, :], recs[2, :], affs[0], affs[1], affs[2], dose, CD25=IL2Ra))
+
+        data = {'Cell Type': [cell],
+            'IL2Rb Bound': [IL2RbBound]
+        }
+        df_temp = pd.DataFrame(data, columns=['Cell Type', 'IL2Rb Bound'])
+        df = df.append(df_temp, ignore_index=True)
+    
+    return df
 
