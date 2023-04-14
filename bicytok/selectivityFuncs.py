254d756a562cf2889712d315e83c69998f4908e0
Aaron Meyer <ameyer@ucla.edu>
2023-04-13 20:57:00 -0700

    Minor cleanup

--- bicytok/selectivityFuncs.py
+++ bicytok/selectivityFuncs.py
@@ -173,27 +173,21 @@ def bindingCalc(df: pd.DataFrame, targCell: string, offTCells: list, betaAffs: n
 bispecOpt_Vec = np.vectorize(cytBindingModel_bispecOpt)
 
 
-def minSelecFunc(x: float, targRecs: np.array, offTRecs: np.array, dose: float, IL2Ra: bool):
+def minSelecFunc(recXaff: float, targRecs: np.array, offTRecs: np.array, dose: float, IL2Ra: bool) -> float:
     """Serves as the function which will have its return value minimized to get optimal selectivity
     To be used in conjunction with optimizeDesign()
     Args:
-        x: receptor affinity which is modulated in optimize design
+        recXaff: receptor affinity which is modulated in optimize design
 
     Return:
         selectivity: value will be minimized, defined as ratio of off target to on target signaling
     """
-    minSelecFunc.targetBound = 0
-    offTargetBound = 0
-
-    recXaff = x
-
     minSelecFunc.targetBound = np.sum(bispecOpt_Vec(targRecs[0, :], targRecs[1, :], targRecs[2, :], recXaff[0], recXaff[1], recXaff[2], dose, CD25=IL2Ra))
     offTargetBound = np.sum(bispecOpt_Vec(offTRecs[0, :], offTRecs[1, :], offTRecs[2, :], recXaff[0], recXaff[1], recXaff[2], dose, CD25=IL2Ra))
     minSelecFunc.targetBound /= targRecs.shape[0]
     offTargetBound /= offTRecs.shape[0]
 
-    selectivity = (offTargetBound) / (minSelecFunc.targetBound)
-    return selectivity
+    return offTargetBound / minSelecFunc.targetBound
 
 
 def optimizeDesign(targCell: string, offTCells: list, selectedDF: pd.DataFrame, epitope: string, dose: float, prevOptAffs: list):
