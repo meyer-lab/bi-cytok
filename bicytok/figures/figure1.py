16ca86a960a0bc97ddc4b8dbb6707d4f70a36be4
Helen <helenk@aretha.seas.ucla.edu>
2023-04-26 16:21:57 -0700

    Figure1 changed to linear regression model

--- bicytok/figures/figure1.py
+++ bicytok/figures/figure1.py
@@ -11,21 +11,21 @@ import matplotlib.pyplot as plt
 
 path_here = dirname(dirname(__file__))
 
-def linregression(params, Xs):
-    A, B = params
-    Ys = A*Xs+B
-    return Ys
-def plotLin(Xs, Ys, ax):
-    sns.lineplot(x=Xs, y=Ys, ax=ax)
 
-params = np.array([2,1])
-Xs = np.linspace(0,10, 100)
-Ys = linregression(params, Xs)
-fig, ax = plt.subplots()
-plotLin(Xs, Ys, ax)
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_title('Linear Regression')
-plt.show()
+def makeFigure():
+    ax, f = getSetup((8, 8), (2, 2))
+    X = np.arange(0,100)
+    Y = np.arange(0,100)
+    def linregression(params, Xs):
+       A, B = params
+       Ys = A*Xs + B
+       return Ys
+    def plotLin(Xs, Ys, ax):
+        sns.lineplot(x=Xs, y=Ys, ax=ax)
     
+    params = np.array([2,1])
+    Xs = np.linspace(0,10, 100)
+    Ys = linregression(params, Xs)
+    plotLin(Xs, Ys, 0)
+    return f
 
