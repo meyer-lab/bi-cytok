e6edb67bd9ad320897c1ba1b7c490e60348b44b3
Helen <helenk@aretha.seas.ucla.edu>
2023-04-30 23:48:05 -0700

    finished practice, array sizes not compatible

--- bicytok/figures/figure1.py
+++ bicytok/figures/figure1.py
@@ -7,6 +7,8 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import minimize
+
 
 
 path_here = dirname(dirname(__file__))
@@ -14,10 +16,12 @@ path_here = dirname(dirname(__file__))
 
 def makeFigure():
     ax, f = getSetup((8, 8), (2, 2))
+    
     def linregression(params, Xs):
        A, B = params
        Ys = A*Xs + B
        return Ys
+    
     def plotLin(Xs, Ys, ax):
         sns.lineplot(x=Xs, y=Ys, ax=ax)
     
@@ -25,5 +29,24 @@ def makeFigure():
     Xs = np.linspace(0,10, 100)
     Ys = linregression(params, Xs)
     plotLin(Xs, Ys, ax[0])
+    
+    x = np.arange(0, 101)
+    y = 2*x + 5
+    noise = np.random.normal(loc=0, scale=1, size=len(x))
+    print(y.shape, noise.shape)
+    y_noisy = y + noise
+
+    def residuals(params, X, Y):
+        A, B = params
+        model_predictions = A*X + B
+        error = model_predictions - Y
+        return error
+    
+    ogparams = [0,0]
+    optimized = minimize(residuals, ogparams, args=(x, y_noisy))
+    optimized_params = optimized.x
+    ax[1].scatter(x, y_noisy, label='Simulated data')
+    ax[1].plot(x, optimized_params[0]*x + optimized_params[1], 'r-', label='Fitted line')
+    ax[1].legend()
     return f
 
