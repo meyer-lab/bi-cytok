7c6aa452a341d92d6b9d8c02eaaa5348a48ea739
Helen <helenk@aretha.seas.ucla.edu>
2023-06-04 23:30:55 -0700

    figure 2

---
+++ bicytok/figures/figure2.py
@@ -0,0 +1,50 @@
+from os.path import dirname, join
+from .common import getSetup
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import least_squares
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import KFold
+from sklearn.metrics import r2_score
+from ..BindingMod import polyc
+from ..imports import importCITE
+
+path_here = dirname(dirname(__file__))
+
+
+def makeFigure(): 
+    df = pd.DataFrame(importCITE())
+    print (df)
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 6))
+
+    # Plot IL2Ra distribution
+    sns.boxplot(x='CellType1', y='CD25', data=df, ax=axes[0])
+    axes[0].set_title('IL2Ra (CD25) Distribution')
+    axes[0].set_xlabel('Cell Type')
+    axes[0].set_ylabel('IL2Ra (CD25)')
+
+    # Plot IL2RB distribution
+    sns.boxplot(x='CellType1', y='CD122', data=df, ax=axes[1])
+    axes[1].set_title('IL2RB (CD122) Distribution')
+    axes[1].set_xlabel('Cell Type')
+    axes[1].set_ylabel('IL2RB (CD122)')   
+    
+    def find_best_markers(cell_type, df):
+        filtered_df = df[df['CellType1'] == cell_type]
+        mean_expression = filtered_df.iloc[:, 2:].mean()
+        mean_expression = mean_expression.drop(['CD25', 'CD122'])
+        rest_of_cells = df[df['CellType1'] != cell_type]
+        mean_expression_rest = rest_of_cells.iloc[:, 2:].mean()
+        fold_change = mean_expression / mean_expression_rest
+        sorted_markers = fold_change.sort_values(ascending=False)
+        top_markers = sorted_markers.head(3)
+        return top_markers.index.tolist()
+
+    cell_type = 'NK'
+    top_markers = find_best_markers(cell_type, df)
+    print(f"The top markers for {cell_type} are: {top_markers}")
+
+    return fig
