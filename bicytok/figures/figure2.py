46b9b5d84b4c6cd86a6eb238b0200aad9760d2c2
Helen <helenk@aretha.seas.ucla.edu>
2023-06-13 22:48:21 -0700

    doing my best

--- bicytok/figures/figure2.py
+++ bicytok/figures/figure2.py
@@ -5,6 +5,7 @@ import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import least_squares
+from scipy.optimize import minimize_scalar
 from sklearn.cross_decomposition import PLSRegression
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import KFold
@@ -47,4 +48,36 @@ def makeFigure():
     top_markers = find_best_markers(cell_type, df)
     print(f"The top markers for {cell_type} are: {top_markers}")
 
+    def optimize_binding_affinity(cell_type, marker_name):
+            # Step A: Grab a random sample of 1000 cells from the CITE-seq dataset
+        df_sample = df.sample(n=1000, random_state=42)  # Adjust the random_state as desired
+
+        # Step B: Calculate IL2RB amount and marker amount for each cell individually
+        il2rb_amounts = df_sample[df_sample['CellType1'] == cell_type]['CD122'] * 1000
+        marker_amounts = df_sample[df_sample['CellType1'] == cell_type][marker_name] * 1000
+
+        # Step C: Use optimization to determine the best affinity
+        best_affinity = None
+        best_ratio = -float('inf')
+
+        def binding_residuals(affinity):
+            # Run binding model 1000 times and calculate IL2RB bound ratios
+            il2rb_bound = []
+            marker_bound = []
+            for _ in range(1000):
+                il2rb_bound_val, marker_bound_val, _ = polyc(1, 1e-12, il2rb_amounts, [1], [1], np.array([1e8, affinity]))
+                il2rb_bound.append(il2rb_bound_val[0])
+                marker_bound.append(marker_bound_val[0])
+
+            # Calculate IL2RB bound ratio
+            ratio = sum(il2rb_bound) / sum(marker_bound)
+            return 1 - ratio  # Minimize the residuals
+
+        # Perform optimization with bounds on affinity
+        result = minimize_scalar(binding_residuals, bounds=(1e5, 1e10), method='bounded')
+        best_affinity = result.x
+        best_ratio = 1 - result.fun
+
+        return best_affinity, best_ratio
+    
     return fig
