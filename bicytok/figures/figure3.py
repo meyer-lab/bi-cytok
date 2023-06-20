d827fb52c0e0209e94a2b350d1ce4afa0a5753da
Helen <helenk@aretha.seas.ucla.edu>
2023-06-20 07:46:28 -0700

    slay

--- bicytok/figures/figure3.py
+++ bicytok/figures/figure3.py
@@ -20,14 +20,6 @@ def makeFigure():
     secondaryAff = 6.0
     valency = 2
 
-    wtIL2RaAff = 8.222
-    wtSecondaryAff = 7.65072247
-    wtEpitopeAff = 9.14874165
-
-    cells1 = np.array(['Treg', 'CD8 Naive', 'NK', 'CD8 TEM', 'CD4 Naive', 'CD4 CTL', 'CD8 TCM', 'CD4 TEM', 'NK Proliferating',
-    'NK_CD56bright'])
-    cells2 = np.array(['Treg Memory', 'Treg Naive'])
-
     # Epitoppes list lets the receptor function know what to grab - you can alter this to include CD335
 
     epitopesList = pd.read_csv(join(path_here, "data/epitopeList.csv"))
@@ -48,11 +40,11 @@ def makeFigure():
     prevOptAffs = [8.0, 8.0, 8.0]
 
     for dose in doseVec:
-        #this function gets the optimal affinties and returns the optimal selectivity (first output), and affinities (second outputs)
-        optParams = optimizeDesign(secondary, epitope, targCell, offTCells, epitopesDF, 1e-9, valency, prevOptAffs)
+        #this function gets the optimal affinties and returns the optimal selectivity (first output), and affinities (second outputs) plot these dose on bottom for all, 2 have selectivity, 2 have affinity 
+        optParams = optimizeDesign(secondary, epitope, targCell, offTCells, epitopesDF, dose, valency, prevOptAffs)
 
     # Plot the optimal affinity and optimal selectivity you get at each dose
 
-    # Then do the same thing but for higher valency (valency = 4)
+    # Then do the same thing but for higher valency (valency = 4) - refers to bi or tet
 
     return f
