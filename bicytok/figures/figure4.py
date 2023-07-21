c0fa2bda65f26e6f51e41c1f776ebf8e1b3043c3
Helen <helenk@aretha.seas.ucla.edu>
2023-07-21 09:13:44 -0700

    testing wass kl

--- bicytok/figures/figure4.py
+++ bicytok/figures/figure4.py
@@ -9,6 +9,7 @@ from .common import EMD_2D
 from .common import EMD_Distribution_Plot
 from .common import EMD_1D
 from .common import EMD1Dvs2D_Analysis
+from .common import Wass_KL_Dist
 from ..imports import importCITE
 
 
@@ -20,12 +21,15 @@ def makeFigure():
     new_df = markerDF.head(1000) 
     ax, f = getSetup((10, 10), (3, 2))
     target_cells = 'Treg' 
+    
     signaling_receptor = 'CD122' 
     non_siganling_receptor = 'CD25'
     receptor_names = ['CD4-1', 'CD27', 'CD3-2', 'CD25', 'CD278'] 
+    x = Wass_KL_Dist(ax[0:2], target_cells, 5)
+    print(x)
     # EMD_2D(new_df, signaling_receptor, target_cells, ax[0])
     # EMD_Distribution_Plot(ax[2], new_df, signaling_receptor, non_siganling_receptor, target_cells)
-    # EMD_1D(new_df, target_cells, ax[1])
-    EMD1Dvs2D_Analysis (receptor_names, target_cells, signaling_receptor, new_df, ax[0], ax[1], ax[2], ax[3])
+    # EMD_1D(new_df, target_cells, ax[3])
+    # EMD1Dvs2D_Analysis (receptor_names, target_cells, signaling_receptor, new_df, ax[0], ax[1], ax[2], ax[3])
     
     return f
