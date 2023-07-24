9928dded10e00ef6467b39a940f1b10ed31f94eb
Helen <helenk@aretha.seas.ucla.edu>
2023-07-23 22:47:18 -0700

    final changes

--- bicytok/figures/figure4.py
+++ bicytok/figures/figure4.py
@@ -20,14 +20,14 @@ def makeFigure():
     markerDF = importCITE()
     new_df = markerDF.head(1000) 
     ax, f = getSetup((10, 10), (3, 2))
-    target_cells = 'Treg Memory' 
+    target_cells = 'Treg' 
     signaling_receptor = 'CD122'  
     non_siganling_receptor = 'CD25'
     receptor_names_top = ['CD25', 'Notch-2', 'CD4-1', 'CD27', 'CD278']
     receptor_names_varried = ['CD25', 'CD109', 'CD27', 'TIGIT', 'CD28']
     # EMD_2D(new_df, signaling_receptor, target_cells, ax[0])
     # EMD_Distribution_Plot(ax[2], new_df, signaling_receptor, non_siganling_receptor, target_cells)
-    EMD_1D(new_df, target_cells, ax[3]) 
-    # EMD1Dvs2D_Analysis (receptor_names_top, target_cells, signaling_receptor, new_df, ax[0], ax[1], ax[2], ax[3])
+    # EMD_1D(new_df, target_cells, ax[3]) 
+    EMD1Dvs2D_Analysis (receptor_names_varried, target_cells, signaling_receptor, new_df, ax[0], ax[1], ax[2], ax[3])
        
     return f 
