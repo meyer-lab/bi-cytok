8cf3689da8ba1b69d7ce89b3e7c3d41e063f9dd4
Helen <helenk@aretha.seas.ucla.edu>
2023-07-24 09:27:24 -0700

    various samples of citeseq

--- bicytok/figures/figure4.py
+++ bicytok/figures/figure4.py
@@ -19,15 +19,17 @@ path_here = dirname(dirname(__file__))
 def makeFigure():  
     markerDF = importCITE()
     new_df = markerDF.head(1000) 
+    new_df1 = markerDF.sample(n=1000, random_state=42)
+    new_df2 = markerDF.sample(n=1000, random_state=12)
     ax, f = getSetup((10, 10), (3, 2))
     target_cells = 'Treg' 
     signaling_receptor = 'CD122'  
     non_siganling_receptor = 'CD25'
     receptor_names_top = ['CD25', 'Notch-2', 'CD4-1', 'CD27', 'CD278']
     receptor_names_varried = ['CD25', 'CD109', 'CD27', 'TIGIT', 'CD28']
-    # EMD_2D(new_df, signaling_receptor, target_cells, ax[0])
+    EMD_2D(new_df2, signaling_receptor, target_cells, ax[0])
     # EMD_Distribution_Plot(ax[2], new_df, signaling_receptor, non_siganling_receptor, target_cells)
     # EMD_1D(new_df, target_cells, ax[3]) 
-    EMD1Dvs2D_Analysis (receptor_names_varried, target_cells, signaling_receptor, new_df, ax[0], ax[1], ax[2], ax[3])
+    # EMD1Dvs2D_Analysis (receptor_names_varried, target_cells, signaling_receptor, new_df, ax[0], ax[1], ax[2], ax[3])
        
     return f 
