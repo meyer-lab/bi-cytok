d8edd94d48b7b24ea81ea8aba371210dfe34f3e7
Helen <helenk@aretha.seas.ucla.edu>
2023-07-23 22:34:22 -0700

    final fixes all good plotting

--- bicytok/figures/common.py
+++ bicytok/figures/common.py
@@ -350,9 +350,7 @@ def EMD1Dvs2D_Analysis(receptor_names, target_cells, signal_receptor, dataset, a
     data_2D_distances = [data[1] for data in filtered_data_2D]
     selectivity_distances = [data[1] for data in filtered_data_selectivity]
     data_names = [data[0] for data in filtered_data_1D]
-    print('1d', data_1D_distances)
-    print('2d', data_2D_distances)
-    print('selec', selectivity_distances)
+   
     # why are only 3 vals being added^^^
     ax3.scatter(data_1D_distances, selectivity_distances, color='blue', label='filtered_data_1D')
     for x, y, name in zip(data_1D_distances, selectivity_distances, data_names):
--- bicytok/figures/figure4.py
+++ bicytok/figures/figure4.py
@@ -20,15 +20,14 @@ def makeFigure():
     markerDF = importCITE()
     new_df = markerDF.head(1000) 
     ax, f = getSetup((10, 10), (3, 2))
-    target_cells = 'Treg' 
+    target_cells = 'Treg Memory' 
     signaling_receptor = 'CD122'  
     non_siganling_receptor = 'CD25'
-    receptor_names_neartop = ['CD4-2', 'CD25', 'CD267', 'CD4-1', 'CD272'] 
-    receptor_names_top = ['CD4-2', 'CD25', 'CD140a', 'CD267', 'CD252']
-    receptor_names_varried = ['CD4-2', 'CD25', 'CD4-1', 'CD59', 'CD26-2']
+    receptor_names_top = ['CD25', 'Notch-2', 'CD4-1', 'CD27', 'CD278']
+    receptor_names_varried = ['CD25', 'CD109', 'CD27', 'TIGIT', 'CD28']
     # EMD_2D(new_df, signaling_receptor, target_cells, ax[0])
     # EMD_Distribution_Plot(ax[2], new_df, signaling_receptor, non_siganling_receptor, target_cells)
-    # EMD_1D(new_df, target_cells, ax[3]) 
-    EMD1Dvs2D_Analysis (receptor_names_varried, target_cells, signaling_receptor, new_df, ax[0], ax[1], ax[2], ax[3])
+    EMD_1D(new_df, target_cells, ax[3]) 
+    # EMD1Dvs2D_Analysis (receptor_names_top, target_cells, signaling_receptor, new_df, ax[0], ax[1], ax[2], ax[3])
        
     return f 
