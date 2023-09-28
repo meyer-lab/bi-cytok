9b95f1f2d600efe3cec903555f7caa63e94ee296
Helen <helenk@aretha.seas.ucla.edu>
2023-09-28 11:32:55 -0700

    new distribution plots

--- bicytok/figures/figure4.py
+++ bicytok/figures/figure4.py
@@ -27,21 +27,26 @@ path_here = dirname(dirname(__file__))
 
 def makeFigure():  
     markerDF = importCITE()
-    new_df = markerDF.head(100)
+    new_df = markerDF.head(1000)
     receptors = []
     for column in new_df.columns:
         if column not in ['CellType1', 'CellType2', 'CellType3', 'Cell']:
             receptors.append(column)
     ax, f = getSetup((40, 40), (1,1)) 
     target_cells = 'Treg' 
-    '''
+    
     cd8_t_df = new_df[new_df['CellType1'] == 'CD8 T']
+    off_target_df = new_df[new_df['CellType1'] != 'CD8 T'] 
+    
     receptor_columns = ['CD57', 'CD8']
     receptor_df = cd8_t_df[receptor_columns]
+    receptor_df_off_target = off_target_df[receptor_columns]
     ######################################################
-    sns.kdeplot(receptor_df['CD57'], ax=ax[0], label='CD57', shade=True)
-    sns.kdeplot(receptor_df['CD8'], ax=ax[0], label='CD8', shade=True)
-    ax[0].set_title('Receptor Distributions for CD8 T Cells')
+    sns.kdeplot(cd8_t_df['CD57'], ax=ax[0], label='CD57 (CD8 T)', shade=True)
+    sns.kdeplot(cd8_t_df['CD8'], ax=ax[0], label='CD8 (CD8 T)', shade=True)
+    sns.kdeplot(receptor_df_off_target['CD57'], ax=ax[0], label='CD57 (Off-Target)', shade=True)
+    sns.kdeplot(receptor_df_off_target['CD8'], ax=ax[0], label='CD8 (Off-Target)', shade=True)
+    ax[0].set_title('Receptor Distributions for CD8 T Cells and Off-Target Cells')    
     ax[0].set_xlabel('Receptor Expression')
     ax[0].legend()
     '''
@@ -65,7 +70,7 @@ def makeFigure():
     # ax[0].set_title('EMD Heatmap')
     ######################################################
     '''
-   
+    '''
     resultsKL = []
     for receptor in receptors:
         val = KL_divergence_2D(new_df, receptor, target_cells, ax = None) 
@@ -85,7 +90,7 @@ def makeFigure():
     ax[0].set_ylabel('Receptor')
     ax[0].set_title('KL Heatmap')
     # f = KLD_clustermap(pivot_tableKL)
-    '''
-    f = EMD_clustermap(pivot_table)
     
+    # f = EMD_clustermap(pivot_table)
+    '''
     return f     
