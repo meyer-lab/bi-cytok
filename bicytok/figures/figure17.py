b0642074954759c088af6220fbc082b859d9a4fc
EmilyTLin <powerless300@gmail.com>
2023-11-13 16:44:48 -0800

    Make distance metric vs. selectivity plots use the same cell dataframe and graph multiple valencies.

--- bicytok/figures/figure17.py
+++ bicytok/figures/figure17.py
@@ -13,13 +13,14 @@ path_here = dirname(dirname(__file__))
 def makeFigure():
     ax, f = getSetup((9, 3), (1, 3))
 
-    markerDF = importCITE()
-    new_df = markerDF.head(1000)
-# Make it show the curve for different valencies and we'll see how it matters less
-    signal = ['CD122', 1]
-    allTargets = [[('CD25', 1), ('CD278', 1)], [('CD25', 1), ('CD4-2', 1)], [('CD25', 1), ('CD45RB', 1)], [('CD25', 1), ('CD81', 1)],
-        [('CD278', 1), ('CD4-2', 1)], [('CD278', 1), ('CD45RB', 1)], [('CD278', 1), ('CD81', 1)], [('CD4-2', 1), ('CD45RB', 1)],
-        [('CD4-2', 1), ('CD81', 1)], [('CD45RB', 1), ('CD81', 1)]]
+    CITE_DF = importCITE()
+    new_df = CITE_DF.sample(1000, random_state=42)
+
+    signal_receptor = 'CD122'
+    signal_valency = 1
+    valencies = [1, 2, 3, 4]
+    allTargets = [['CD25', 'CD278'], ['CD25', 'CD4-2'], ['CD25', 'CD45RB'], ['CD25', 'CD81'], ['CD278', 'CD4-2'],
+        ['CD278', 'CD45RB'], ['CD278', 'CD81'], ['CD4-2', 'CD45RB'], ['CD4-2', 'CD81'], ['CD45RB', 'CD81']]
     dose = 10e-2
 
     cells = np.array(['CD8 Naive', 'NK', 'CD8 TEM', 'CD4 Naive', 'CD4 CTL', 'CD8 TCM', 'CD8 Proliferating',
@@ -31,36 +32,35 @@ def makeFigure():
     epitopes = list(epitopesList['Epitope'].unique())
     epitopesDF = getSampleAbundances(epitopes, cells)
 
-    df = pd.DataFrame(columns=['KL Divergence', "Earth Mover's Distance", 'Correlation', 'Selectivity'])
+    df = pd.DataFrame(columns=['KL Divergence', "Earth Mover's Distance", 'Correlation', 'Selectivity', 'Valency'])
 
-    for targetPairs in allTargets:
-        prevOptAffs = [8.0, 8.0, 8.0]
+    for val in valencies:
+        for targets in allTargets:
+            prevOptAffs = [8.0, 8.0, 8.0]
 
-        valencies = [signal[1]]
-        targets = []
-        for target, valency in targetPairs:
-            targets.append(target)
-            valencies.append(valency)
+            vals = [signal_valency, val, val]
 
-        optParams = optimizeDesign(signal[0], targets, targCell, offTCells, epitopesDF, dose, valencies, prevOptAffs)
-        prevOptAffs = [optParams[1][0], optParams[1][1], optParams[1][2]]
+            optParams = optimizeDesign(signal_receptor, targets, targCell, offTCells, epitopesDF, dose, vals, prevOptAffs)
+            prevOptAffs = [optParams[1][0], optParams[1][1], optParams[1][2]]
 
-        KLD = KL_divergence_2D_pair(new_df, targCell, targets[0], targets[1])
-        EMD = EMD_2D_pair(new_df, targCell, targets[0], targets[1])
-        corr = correlation(targCell, targets).loc[targets[0], targets[1]]
-            
-        data = {'KL Divergence': [KLD],
-            "Earth Mover's Distance": [EMD],
-            'Correlation': [corr],
-            'Selectivity': 1 / optParams[0]
-        }
-        df_temp = pd.DataFrame(data, columns=['KL Divergence', "Earth Mover's Distance", 'Correlation', 'Selectivity'])
-        df = pd.concat([df, df_temp], ignore_index=True)
+            KLD = KL_divergence_2D_pair(new_df, targCell, targets[0], targets[1])
+            EMD = EMD_2D_pair(new_df, targCell, targets[0], targets[1])
+            corr = correlation(targCell, targets).loc[targets[0], targets[1]]
+                
+            data = {'KL Divergence': [KLD],
+                "Earth Mover's Distance": [EMD],
+                'Correlation': [corr],
+                'Selectivity': 1 / optParams[0],
+                'Valency': val
+            }
+            df_temp = pd.DataFrame(data, columns=['KL Divergence', "Earth Mover's Distance", 'Correlation', 'Selectivity', 'Valency'])
+            df = pd.concat([df, df_temp], ignore_index=True)
 
-    sns.lineplot(data=df, x='KL Divergence', y='Selectivity', ax=ax[0])
-    sns.lineplot(data=df, x="Earth Mover's Distance", y='Selectivity', ax=ax[1])
-    sns.lineplot(data=df, x='Correlation', y='Selectivity', ax=ax[2])
+    sns.lineplot(data=df, x='KL Divergence', y='Selectivity', hue='Valency', ax=ax[0])
+    sns.lineplot(data=df, x="Earth Mover's Distance", y='Selectivity', hue='Valency', ax=ax[1])
+    sns.lineplot(data=df, x='Correlation', y='Selectivity', hue='Valency', ax=ax[2])
     ax[0].set(xscale='log')
     ax[1].set(xscale='log')
+    ax[2].set(xscale='log')
 
     return f
