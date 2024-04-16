e566049331f74dc96a5f822b04401105013ac56d
EmilyTLin <powerless300@gmail.com>
2024-04-15 20:24:03 -0700

    Update test_model to align with new optimizeDesign function for multivalency and multispecificity.

--- bicytok/test/test_model.py
+++ bicytok/test/test_model.py
@@ -5,7 +5,6 @@ import numpy as np
 import pandas as pd
 from bicytok.selectivityFuncs import getSampleAbundances, optimizeDesign
 
-
 def test_optimize_design():
     targCell = "Treg"
     offTCells = ["CD8 Naive", "NK", "CD8 TEM", "CD4 Naive", "CD4 CTL"]
@@ -16,11 +15,12 @@ def test_optimize_design():
     epitopesDF = getSampleAbundances(epitopes, cells)
 
     optimizeDesign(
-        secondary="CD122",
-        epitope="CD25",
+        signal="CD122",
+        targets=["CD25"],
         targCell=targCell,
-        offTCells=offTCells,
+        offTCells=[offTCells],
         selectedDF=epitopesDF,
         dose=0.1,
-        valency=2,
+        valencies=[2,2],
+        prevOptAffs=[8.0,8.0],
     )
