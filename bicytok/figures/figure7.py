c61c81f7cfab3abe4185ded6f7bae74269d05b6f
Aaron Meyer <ameyer@ucla.edu>
2024-05-16 14:50:12 -0700

    Format figures 7 and 8

--- bicytok/figures/figure7.py
+++ bicytok/figures/figure7.py
@@ -1,33 +1,40 @@
-from .common import getSetup
 import pandas as pd
 import seaborn as sns
-import numpy as np
+from .common import getSetup
 from ..distanceMetricFuncs import EMD_2D
 from ..imports import importCITE
 
 
-def makeFigure():  
-    ''' clustermaps of EMD values for receptors + specified cell type'''
+def makeFigure():
+    """clustermaps of EMD values for receptors + specified cell type"""
     markerDF = importCITE()
     new_df = markerDF.head(1000)
     receptors = []
     for column in new_df.columns:
-        if column not in ['CellType1', 'CellType2', 'CellType3', 'Cell']:
+        if column not in ["CellType1", "CellType2", "CellType3", "Cell"]:
             receptors.append(column)
-    ax, f = getSetup((40, 40), (1,1)) 
-    target_cells = "Treg" 
-   
+    ax, f = getSetup((40, 40), (1, 1))
+    target_cells = "Treg"
+
     # Clustermap for EMD
     resultsEMD = []
-    receptors = ['CD25', 'CD35']
+    receptors = ["CD25", "CD35"]
     for receptor in receptors:
-        val = EMD_2D(new_df, receptor, target_cells, special_receptor = None, ax = None) 
+        val = EMD_2D(new_df, receptor, target_cells, special_receptor=None, ax=None)
         resultsEMD.append(val)
-    flattened_results = [result_tuple for inner_list in resultsEMD for result_tuple in inner_list]
+    flattened_results = [
+        result_tuple for inner_list in resultsEMD for result_tuple in inner_list
+    ]
     # Create a DataFrame from the flattened_results
-    df_recep = pd.DataFrame(flattened_results, columns=['Distance', 'Receptor', 'Signal Receptor'])
-    pivot_table = df_recep.pivot_table(index='Receptor', columns='Signal Receptor', values='Distance')
+    df_recep = pd.DataFrame(
+        flattened_results, columns=["Distance", "Receptor", "Signal Receptor"]
+    )
+    pivot_table = df_recep.pivot_table(
+        index="Receptor", columns="Signal Receptor", values="Distance"
+    )
     dataset = pivot_table.fillna(0)
-    f =  (sns.clustermap(dataset, cmap='bwr', figsize=(10,10), annot_kws={'fontsize': 16}))
-    
-    return f     
+    f = sns.clustermap(
+        dataset, cmap="bwr", figsize=(10, 10), annot_kws={"fontsize": 16}
+    )
+
+    return f
--- bicytok/figures/figure8.py
+++ bicytok/figures/figure8.py
@@ -1,32 +1,41 @@
-from .common import getSetup
 import pandas as pd
 import seaborn as sns
-import numpy as np
+from .common import getSetup
 from ..distanceMetricFuncs import KL_divergence_2D
 from ..imports import importCITE
 
 
-def makeFigure():  
-    ''' clustermaps of KL values for receptors + specified cell type'''
+def makeFigure():
+    """clustermaps of KL values for receptors + specified cell type"""
     markerDF = importCITE()
     new_df = markerDF.head(1000)
     receptors = []
     for column in new_df.columns:
-        if column not in ['CellType1', 'CellType2', 'CellType3', 'Cell']:
+        if column not in ["CellType1", "CellType2", "CellType3", "Cell"]:
             receptors.append(column)
-    ax, f = getSetup((40, 40), (1,1)) 
-    receptors = ['CD25', 'CD35']
-    target_cells = "Treg" 
+    ax, f = getSetup((40, 40), (1, 1))
+    receptors = ["CD25", "CD35"]
+    target_cells = "Treg"
     resultsKL = []
     for receptor in receptors[0:5]:
-        val = KL_divergence_2D(new_df, receptor, target_cells, special_receptor = None, ax = None) 
-        resultsKL.append(val) 
-    flattened_resultsKL = [result_tuple for inner_list in resultsKL for result_tuple in inner_list]
+        val = KL_divergence_2D(
+            new_df, receptor, target_cells, special_receptor=None, ax=None
+        )
+        resultsKL.append(val)
+    flattened_resultsKL = [
+        result_tuple for inner_list in resultsKL for result_tuple in inner_list
+    ]
 
     # Create a DataFrame from the flattened_results
-    df_recep = pd.DataFrame(flattened_resultsKL, columns=['KLD', 'Receptor', 'Signal Receptor'])
-    pivot_tableKL = df_recep.pivot_table(index='Receptor', columns='Signal Receptor', values='KLD')
+    df_recep = pd.DataFrame(
+        flattened_resultsKL, columns=["KLD", "Receptor", "Signal Receptor"]
+    )
+    pivot_tableKL = df_recep.pivot_table(
+        index="Receptor", columns="Signal Receptor", values="KLD"
+    )
     dataset = pivot_tableKL.fillna(0)
-    f = (sns.clustermap(dataset, cmap='bwr', figsize=(10,10), annot_kws={'fontsize': 16}))
-    
-    return f   
+    f = sns.clustermap(
+        dataset, cmap="bwr", figsize=(10, 10), annot_kws={"fontsize": 16}
+    )
+
+    return f
