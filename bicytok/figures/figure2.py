74c44cf4af9e92f60784924559f9893cf5d88502
Helen <helenk@aretha.seas.ucla.edu>
2024-10-31 13:49:23 -0700

    figure 2 plot and filter first draft

--- bicytok/figures/figure2.py
+++ bicytok/figures/figure2.py
@@ -1,45 +1,58 @@
 from ..distanceMetricFuncs import KL_EMD_1D
+from os.path import dirname
+import numpy as np
+from imports import importCITE
 from .common import getSetup
 
+path_here = dirname(dirname(__file__))
 
 def makeFigure():
     """Figure file to generate 1D KL divergence and EMD for given cell type/subset."""
-    ax, f = getSetup((8, 8), (3, 2))
+    ax, f = getSetup((8, 8), (1, 2))
 
-    KL_EMD_1D(ax[0:2], "Treg Memory", 10)
-    KL_EMD_1D(ax[2:4], "Treg Memory", 10, offTargState=1)
-    KL_EMD_1D(ax[4:6], "Treg Memory", 10, offTargState=2)
-    ''' filtering data portion 
-    CITE_DF = importCITE()
-    markerDF = pd.DataFrame(columns=["Marker", "Cell Type", "Amount"])
-    for marker in CITE_DF.loc[
-        :,
-        (
-            (CITE_DF.columns != "CellType1")
-            & (CITE_DF.columns != "CellType2")
-            & (CITE_DF.columns != "CellType3")
-            & (CITE_DF.columns != "Cell")
-        ),
-    ].columns:
+    targCell = "Treg"
+    offTargState = 0
     
-    '''
+    
+    CITE_DF = importCITE()  
+    
+    # Filter out non-marker columns
+    marker_columns = CITE_DF.columns[
+        (CITE_DF.columns != "CellType1") &
+        (CITE_DF.columns != "CellType2") &
+        (CITE_DF.columns != "CellType3") &
+        (CITE_DF.columns != "Cell")
+    ]
+    markerDF = CITE_DF.loc[:, marker_columns]
 
-    ''' plot portion
-    corrsDF = pd.DataFrame()
-    for i, distance in enumerate(["Wasserstein Distance", "KL Divergence"]):
-        ratioDF = markerDF.sort_values(by=distance)
-        posCorrs = ratioDF.tail(numFactors).Marker.values
-        corrsDF = pd.concat(
-            [corrsDF, pd.DataFrame({"Distance": distance, "Marker": posCorrs})]
-        )
-        markerDF = markerDF.loc[markerDF["Marker"].isin(posCorrs)]
-        sns.barplot(
-            data=ratioDF.tail(numFactors), y="Marker", x=distance, ax=ax[i], color="k"
-        )
-        ax[i].set(xscale="log")
-        ax[0].set(title="Wasserstein Distance - Surface Markers")
-        ax[1].set(title="KL Divergence - Surface Markers")
-    return corrsDF
+    # binary arrays for on-target and off-target 
+    on_target = (CITE_DF["CellType3"] == targCell).astype(int)
+    if offTargState == 0:  # All non-memory Tregs
+        off_target = (CITE_DF["CellType3"] != targCell).astype(int)
+    elif offTargState == 1:  # All non-Tregs
+        off_target = (CITE_DF["CellType2"] != "Treg").astype(int)
+    elif offTargState == 2:  # Naive Tregs
+        off_target = (CITE_DF["CellType3"] == "Treg Naive").astype(int)
+    else:
+        raise ValueError("Invalid offTargState value. Must be 0, 1, or 2.")
     
-    '''
+    
+    KL_values, EMD_values = KL_EMD_1D(markerDF, on_target, off_target)
+    
+    top_5_KL_indices = np.argsort(KL_values)[-5:]
+    top_5_EMD_indices = np.argsort(EMD_values)[-5:]
+    
+    # Plot KL values
+    ax[0].barh(markerDF.columns[top_5_KL_indices], KL_values[top_5_KL_indices], color='b')
+    ax[0].set_title('Top 5 KL Divergence Values')
+    ax[0].set_xlabel('KL Divergence')
+    ax[0].invert_yaxis()  
+
+    # Plot EMD values
+    ax[1].barh(markerDF.columns[top_5_EMD_indices], EMD_values[top_5_EMD_indices], color='g')
+    ax[1].set_title('Top 5 EMD Values')
+    ax[1].set_xlabel('EMD Value')
+    ax[1].invert_yaxis()  
+
+    
     return f
