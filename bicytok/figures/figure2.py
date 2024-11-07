92ab5cac123bb8d191696382b064474e3d6e62b9
Aaron Meyer <ameyer@ucla.edu>
2024-11-06 20:04:09 -0800

    Rye formatting

--- bicytok/figures/figure2.py
+++ bicytok/figures/figure2.py
@@ -1,10 +1,13 @@
-from ..distanceMetricFuncs import KL_EMD_1D
 from os.path import dirname
+
 import numpy as np
 from imports import importCITE
+
+from ..distanceMetricFuncs import KL_EMD_1D
 from .common import getSetup
 
 path_here = dirname(dirname(__file__))
+
 
 def makeFigure():
     """Figure file to generate 1D KL divergence and EMD for given cell type/subset."""
@@ -12,10 +15,9 @@ def makeFigure():
 
     targCell = "Treg"
     offTargState = 0
-    
-    
-    CITE_DF = importCITE()  
-    
+
+    CITE_DF = importCITE()
+
     # Filter out non-marker columns
     non_marker_columns = ["CellType1", "CellType2", "CellType3", "Cell"]
     marker_columns = CITE_DF.columns[~CITE_DF.columns.isin(non_marker_columns)]
@@ -26,9 +28,9 @@ def makeFigure():
 
     # Define off-target conditions using a dictionary
     off_target_conditions = {
-        0: (CITE_DF["CellType3"] != targCell),     # All non-memory Tregs
-        1: (CITE_DF["CellType2"] != "Treg"),       # All non-Tregs
-        2: (CITE_DF["CellType3"] == "Treg Naive")  # Naive Tregs
+        0: (CITE_DF["CellType3"] != targCell),  # All non-memory Tregs
+        1: (CITE_DF["CellType2"] != "Treg"),  # All non-Tregs
+        2: (CITE_DF["CellType3"] == "Treg Naive"),  # Naive Tregs
     }
 
     # Set off_target based on offTargState
@@ -36,24 +38,26 @@ def makeFigure():
         off_target = off_target_conditions[offTargState].astype(int)
     else:
         raise ValueError("Invalid offTargState value. Must be 0, 1, or 2.")
-    
-    
+
     KL_values, EMD_values = KL_EMD_1D(markerDF, on_target, off_target)
-    
+
     top_5_KL_indices = np.argsort(KL_values)[-5:]
     top_5_EMD_indices = np.argsort(EMD_values)[-5:]
-    
+
     # Plot KL values
-    ax[0].barh(markerDF.columns[top_5_KL_indices], KL_values[top_5_KL_indices], color='b')
-    ax[0].set_title('Top 5 KL Divergence Values')
-    ax[0].set_xlabel('KL Divergence')
-    ax[0].invert_yaxis()  
+    ax[0].barh(
+        markerDF.columns[top_5_KL_indices], KL_values[top_5_KL_indices], color="b"
+    )
+    ax[0].set_title("Top 5 KL Divergence Values")
+    ax[0].set_xlabel("KL Divergence")
+    ax[0].invert_yaxis()
 
     # Plot EMD values
-    ax[1].barh(markerDF.columns[top_5_EMD_indices], EMD_values[top_5_EMD_indices], color='g')
-    ax[1].set_title('Top 5 EMD Values')
-    ax[1].set_xlabel('EMD Value')
-    ax[1].invert_yaxis()  
+    ax[1].barh(
+        markerDF.columns[top_5_EMD_indices], EMD_values[top_5_EMD_indices], color="g"
+    )
+    ax[1].set_title("Top 5 EMD Values")
+    ax[1].set_xlabel("EMD Value")
+    ax[1].invert_yaxis()
 
-    
     return f
--- bicytok/figures/figure8.py
+++ bicytok/figures/figure8.py
@@ -1,18 +1,19 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-from .common import getSetup
+
 from ..distanceMetricFuncs import KL_divergence_2D
 from ..imports import importCITE
-import matplotlib.pyplot as plt
+from .common import getSetup
 
 
 def makeFigure():
     """clustermaps of KL values for receptors + specified cell type"""
     CITE_DF = importCITE()
     CITE_DF = CITE_DF.head(1000)
-    
+
     ax, f = getSetup((40, 40), (1, 1))
-    
+
     targCell = "Treg"
     offTargState = 0
 
@@ -23,16 +24,18 @@ def makeFigure():
 
     # Further filter to include only columns related to CD25 and CD35
     receptors_of_interest = ["CD25", "CD35"]
-    filtered_markerDF = markerDF.loc[:, markerDF.columns.str.contains('|'.join(receptors_of_interest), case=False)]
+    filtered_markerDF = markerDF.loc[
+        :, markerDF.columns.str.contains("|".join(receptors_of_interest), case=False)
+    ]
 
     # Binary arrays for on-target and off-target cell types
     on_target = (CITE_DF["CellType3"] == targCell).astype(int)
 
     # Define off-target conditions using a dictionary
     off_target_conditions = {
-        0: (CITE_DF["CellType3"] != targCell),     # All non-memory Tregs
-        1: (CITE_DF["CellType2"] != "Treg"),       # All non-Tregs
-        2: (CITE_DF["CellType3"] == "Treg Naive")  # Naive Tregs
+        0: (CITE_DF["CellType3"] != targCell),  # All non-memory Tregs
+        1: (CITE_DF["CellType2"] != "Treg"),  # All non-Tregs
+        2: (CITE_DF["CellType3"] == "Treg Naive"),  # Naive Tregs
     }
 
     # Set off_target based on offTargState
@@ -40,17 +43,19 @@ def makeFigure():
         off_target = off_target_conditions[offTargState].astype(int)
     else:
         raise ValueError("Invalid offTargState value. Must be 0, 1, or 2.")
-    
+
     kl_matrix = KL_divergence_2D(filtered_markerDF, on_target, off_target)
-    
-    df_recep = pd.DataFrame(kl_matrix, index=receptors_of_interest, columns=receptors_of_interest)
+
+    df_recep = pd.DataFrame(
+        kl_matrix, index=receptors_of_interest, columns=receptors_of_interest
+    )
 
     # Visualize with a clustermap
-    sns.heatmap(df_recep, cmap="bwr", annot=True, ax=ax, cbar=True, annot_kws={"fontsize": 16})
+    sns.heatmap(
+        df_recep, cmap="bwr", annot=True, ax=ax, cbar=True, annot_kws={"fontsize": 16}
+    )
 
     ax.set_title("KL Divergence between: CD25 and CD35")
     plt.show()
-        
-    
 
     return f
