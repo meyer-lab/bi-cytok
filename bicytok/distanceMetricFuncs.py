6e649a12a72e6dffcc1d76c618e7345974bde296
Helen <helenk@aretha.seas.ucla.edu>
2024-12-04 16:36:25 -0800

    distance metric not working

--- bicytok/distanceMetricFuncs.py
+++ bicytok/distanceMetricFuncs.py
@@ -22,7 +22,9 @@ def KL_EMD_1D(
     """
     assert all(
     isinstance(i, (bool, np.bool_)) for i in np.append(targ, offTarg)), "targ and offTarg must contain only boolean values."
-    assert (sum(targ) != 0 and sum(offTarg) != 0)
+    #assert (
+     #   sum(targ) != 0 and sum(offTarg) != 0
+    #) 
     
     KL_div_vals = np.full(recAbundances.shape[1], np.nan)
     EMD_vals = np.full(recAbundances.shape[1], np.nan)
