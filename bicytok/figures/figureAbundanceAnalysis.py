bee9b64d72980b92459abc964adae4bc73521e59
Helen <helenk@aretha.seas.ucla.edu>
2025-03-23 16:40:55 -0700

    abundance analysis complete

--- bicytok/figures/figureAbundanceAnalysis.py
+++ bicytok/figures/figureAbundanceAnalysis.py
@@ -9,149 +9,106 @@ from .common import getSetup
 path_here = Path(__file__).parent.parent
 
 def makeFigure():
-    ax, f = getSetup((14, 14), (1, 2)) 
+    ax, f = getSetup((15, 5), (1, 3))
 
-    receptors_of_interest = [
-    "CD25",
-    "CD278",
-    "CD4-1",
-    "CD27",
-    "CD45RB",
-    "CD28",
-    "TCR-2",
-    "TIGIT",
-    "CD4-2",
-    "CD122",
-    "CD3-1",
-    "CD3-2",
-    "CD146"
-]
-
-    sample_size = 1000
+    # Parameters
     targCell = "Treg"
-    cellTypes = np.array(
-        [
-            "CD8 Naive",
-            "NK",
-            "CD8 TEM",
-            "CD4 Naive",
-            "CD4 CTL",
-            "CD8 TCM",
-            "CD8 Proliferating",
-            "Treg",
-        ]
-    )
-
-    sample_size = 100
+    receptors_of_interest = [
+        "CD25",
+        "CD4-1",
+        "CD27",
+        "CD4-2",
+        "CD278",
+        "CD122",
+        "CD28",
+        "TCR-2",
+        "TIGIT",
+        "TSLPR",
+    ]
+    sample_size = 1000
     cell_categorization = "CellType2"
 
     CITE_DF = importCITE()
 
     assert targCell in CITE_DF[cell_categorization].unique()
 
-    epitopes = [
-        col
-        for col in CITE_DF.columns
-        if col not in ["CellType1", "CellType2", "CellType3"]
-    ]
-    epitopesDF = CITE_DF[epitopes + [cell_categorization]]
+    epitopesDF = CITE_DF[receptors_of_interest + [cell_categorization]]
     epitopesDF = epitopesDF.rename(columns={cell_categorization: "Cell Type"})
     sampleDF = sample_receptor_abundances(
         CITE_DF=epitopesDF,
-        numCells=min(sample_size, epitopesDF.shape[0]),
+        numCells=sample_size,
         targCellType=targCell,
     )
-    
-    
-    filtered_sampleDF = sampleDF.loc[
-        :,
-        sampleDF.columns.str.fullmatch("|".join(receptors_of_interest), case=False),
-    ]
-    raw_sampleDF = epitopesDF.copy()
-    filtered_raw_sampleDF = raw_sampleDF.loc[
-        sampleDF.index,  
-        sampleDF.columns
-    ]
-    print ("sampleDF", sampleDF)
+    rec_abundances = sampleDF[receptors_of_interest].to_numpy()
 
-    print ("raw_sampleDF", raw_sampleDF)
-    
-    
-    print ("filtered_sampleDF", filtered_sampleDF)
+    target_mask = (sampleDF["Cell Type"] == targCell).to_numpy()
+    off_target_mask = ~target_mask
 
-    on_target_mask = (sampleDF["Cell Type"] == targCell).to_numpy()
-    off_target_mask = ~on_target_mask
-
-    rec_abundances = filtered_sampleDF.to_numpy()
-    rec_abundancesraw = filtered_raw_sampleDF.to_numpy()
+    target_cells = epitopesDF[epitopesDF["Cell Type"] == targCell]
+    num_target_cells = min(sample_size, target_cells.shape[0])
+    raw_sampleDF = target_cells.sample(num_target_cells, random_state=42)
+    rec_abundancesraw = raw_sampleDF[receptors_of_interest].to_numpy()
 
     KL_div_vals, EMD_vals = KL_EMD_2D(
-        rec_abundances, on_target_mask, off_target_mask, calc_1D=False
+        rec_abundances, target_mask, off_target_mask, calc_1D=True
     )
-
     KL_div_valsRAW, EMD_valsRAW = KL_EMD_2D(
-        rec_abundancesraw, on_target_mask, off_target_mask, calc_1D=False
+        rec_abundancesraw, target_mask, off_target_mask, calc_1D=True
     )
+    KL_div_valsRAW1, EMD_valsRAW1 = KL_EMD_1D(
+        rec_abundancesraw, target_mask, off_target_mask
+    )
+    
+    KL_div_vals_2Draw = np.diag(KL_div_valsRAW)
+    EMD_vals_2Draw = np.diag(EMD_valsRAW)
 
-    print("EMD_vals", EMD_vals)
-    print("EMD_valsRAW", EMD_valsRAW)
-    print ("KL_div_vals", KL_div_vals)
-    print ("KL_div_valsRAW", KL_div_valsRAW)
-    metrics_raw = {
-        "Receptor Pair": receptors_of_interest,
-        "Data Type": ["Raw"] * len(receptors_of_interest),
-        "KL Divergence": KL_div_valsRAW,
-        "EMD": EMD_valsRAW
+
+    KL_div_vals_2D = np.diag(KL_div_vals)
+    EMD_vals_2D = np.diag(EMD_vals
+    )
+    data = {
+        "Receptor": receptors_of_interest,
+        "KL Divergence RAW 2D": KL_div_vals_2Draw,
+        "EMD RAW 2D": EMD_vals_2Draw,
+        "KL Divergence": KL_div_vals_2D,
+        "EMD": EMD_vals_2D,
+        "KL Divergence RAW 1D": KL_div_valsRAW1,
+        "EMD RAW 1D": EMD_valsRAW1,
     }
-    metrics_df2_raw = pd.DataFrame(metrics_raw)
+    df = pd.DataFrame(data)
+    df_melted_raw = df[["Receptor", "KL Divergence RAW 2D", "EMD RAW 2D"]].melt(id_vars="Receptor", 
+                                                                         value_vars=["KL Divergence RAW 2D", "EMD RAW 2D"], 
+                                                                         var_name="Metric", 
+                                                                         value_name="Value")
 
-    # Prepare the abundance metrics DataFrame
-    metrics_abundance = {
-        "Receptor Pair": receptors_of_interest,
-        "Data Type": ["Abundance"] * len(receptors_of_interest),
-        "KL Divergence": KL_div_vals,
-        "EMD": EMD_vals
-    }
-    metrics_df2_abundance = pd.DataFrame(metrics_abundance)
+    df_melted_abundance = df[["Receptor", "KL Divergence", "EMD"]].melt(id_vars="Receptor", 
+                                                                        value_vars=["KL Divergence", "EMD"], 
+                                                                        var_name="Metric", 
+                                                                        value_name="Value")
+    
+    df_melted_raw1d = df[["Receptor", "KL Divergence RAW 1D", "EMD RAW 1D"]].melt(id_vars="Receptor", 
+                                                                         value_vars=["KL Divergence RAW 1D", "EMD RAW 1D"], 
+                                                                         var_name="Metric", 
+                                                                         value_name="Value")
 
-    # Melt the dataframes for Seaborn plotting
-    metrics_df2_raw_melted = metrics_df2_raw.melt(
-        id_vars=["Receptor Pair", "Data Type"],
-        value_vars=["KL Divergence", "EMD"],
-        var_name="Metric",
-        value_name="Value"
-    )
+    # Plot barplot for RAW data
+    sns.barplot(data=df_melted_raw, x="Receptor", y="Value", hue="Metric", ax=ax[0])
+    ax[0].set_title(f"Receptor KL Divergence and EMD (RAW) 2D")
+    ax[0].set_xlabel("Receptor")
+    ax[0].set_ylabel("Value")
+    ax[0].legend(title="Metric")
 
-    metrics_df2_abundance_melted = metrics_df2_abundance.melt(
-        id_vars=["Receptor Pair", "Data Type"],
-        value_vars=["KL Divergence", "EMD"],
-        var_name="Metric",
-        value_name="Value"
-    )
+    # Plot barplot for Abundance conversion data
+    sns.barplot(data=df_melted_abundance, x="Receptor", y="Value", hue="Metric", ax=ax[1])
+    ax[1].set_title(f"Receptor KL Divergence and EMD (Abundance Conversion) 2D")
+    ax[1].set_xlabel("Receptor")
+    ax[1].set_ylabel("Value")
+    ax[1].legend(title="Metric")
 
-    # Plotting
-    sns.barplot(data=metrics_df2_raw_melted, x='Receptor Pair', y='Value', hue='Metric', ax=ax[0])
-    ax[0].set_title('KL Divergence and EMD for 2D Metrics (Raw Data)')
-    ax[0].set_xticklabels(ax[0].get_xticklabels(), rotation=45, ha='right')
-    ax[0].set_ylabel('Value')
+    sns.barplot(data=df_melted_raw1d, x="Receptor", y="Value", hue="Metric", ax=ax[2])
+    ax[2].set_title(f"Receptor KL Divergence and EMD (RAW) 1D")
+    ax[2].set_xlabel("Receptor")
+    ax[2].set_ylabel("Value")
+    ax[2].legend(title="Metric")
 
-    sns.barplot(data=metrics_df2_abundance_melted, x='Receptor Pair', y='Value', hue='Metric', ax=ax[1])
-    ax[1].set_title('KL Divergence and EMD for 2D Metrics (Receptor Abundance)')
-    ax[1].set_xticklabels(ax[1].get_xticklabels(), rotation=45, ha='right')
-    ax[1].set_ylabel('Value')
-
-    '''
-    sns.barplot(data=metrics_df1_raw.melt(id_vars=["Receptor Pair", "Data Type"], value_vars=["KL Divergence", "EMD"], var_name="Metric", value_name="Value"), 
-                x='Receptor Pair', y='Value', hue='Metric', ax=ax[2])
-    ax[2].set_title('KL Divergence and EMD for 1D Metrics (Raw Data)')
-    ax[2].set_xticklabels(ax[2].get_xticklabels(), rotation=45, ha='right')
-    ax[2].set_ylabel('Value')
-
-    sns.barplot(data=metrics_df1_abundance.melt(id_vars=["Receptor Pair", "Data Type"], value_vars=["KL Divergence", "EMD"], var_name="Metric", value_name="Value"), 
-                x='Receptor Pair', y='Value', hue='Metric', ax=ax[3])
-    ax[3].set_title('KL Divergence and EMD for 1D Metrics (Receptor Abundance)')
-    ax[3].set_xticklabels(ax[3].get_xticklabels(), rotation=45, ha='right')
-    ax[3].set_ylabel('Value')'
-    '''
-
-    return f
+    return f 
