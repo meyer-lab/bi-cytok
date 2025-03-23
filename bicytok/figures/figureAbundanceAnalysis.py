d54e13d2633c0837b66e580084fd9e2e2855ff9a
Helen <helenk@aretha.seas.ucla.edu>
2025-03-23 13:38:21 -0700

    error w raw 2D function not fixed

--- bicytok/figures/figureAbundanceAnalysis.py
+++ bicytok/figures/figureAbundanceAnalysis.py
@@ -62,97 +62,80 @@ def makeFigure():
         targCellType=targCell,
     )
     
-    sampleDFrawvalues = epitopesDF.sample(
-        min(sample_size, epitopesDF.shape[0]), random_state=42
-    )
-
-    # Ensure the final DataFrame has the same format as `sampleDF`, including the 'Cell Type' column
-    sampleDFrawvalues = sampleDFrawvalues.rename(columns={cell_categorization: "Cell Type"})
-
     
     filtered_sampleDF = sampleDF.loc[
         :,
         sampleDF.columns.str.fullmatch("|".join(receptors_of_interest), case=False),
     ]
-    receptors_of_interest = filtered_sampleDF.columns
-    filtered_sampleDFrawvalues = sampleDFrawvalues.loc[
-        :,
-        sampleDFrawvalues.columns.str.fullmatch("|".join(receptors_of_interest), case=False),
+    raw_sampleDF = epitopesDF.copy()
+    filtered_raw_sampleDF = raw_sampleDF.loc[
+        sampleDF.index,  
+        sampleDF.columns
     ]
+    print ("sampleDF", sampleDF)
+
+    print ("raw_sampleDF", raw_sampleDF)
+    
+    
+    print ("filtered_sampleDF", filtered_sampleDF)
 
     on_target_mask = (sampleDF["Cell Type"] == targCell).to_numpy()
     off_target_mask = ~on_target_mask
 
     rec_abundances = filtered_sampleDF.to_numpy()
-    rec_abdundnaces_raw = filtered_sampleDFrawvalues.to_numpy()
+    rec_abundancesraw = filtered_raw_sampleDF.to_numpy()
 
     KL_div_vals, EMD_vals = KL_EMD_2D(
         rec_abundances, on_target_mask, off_target_mask, calc_1D=False
     )
 
-    EMD_matrix = np.tril(EMD_vals, k=0)
-    EMD_matrix = EMD_matrix + EMD_matrix.T - np.diag(np.diag(EMD_matrix))
-    KL_matrix = np.tril(KL_div_vals, k=0)
-    KL_matrix = KL_matrix + KL_matrix.T - np.diag(np.diag(KL_matrix))
-
     KL_div_valsRAW, EMD_valsRAW = KL_EMD_2D(
-        rec_abdundnaces_raw, on_target_mask, off_target_mask, calc_1D=False
+        rec_abundancesraw, on_target_mask, off_target_mask, calc_1D=False
     )
 
-    EMD_matrixraw = np.tril(EMD_valsRAW, k=0)
-    EMD_matrixraw = EMD_matrixraw + EMD_matrixraw.T - np.diag(np.diag(EMD_matrixraw))
-    KL_matrixraw = np.tril(KL_div_valsRAW, k=0)
-    KL_matrixraw = KL_matrixraw + KL_matrixraw.T - np.diag(np.diag(KL_matrixraw))
-    
-    '''
-    metrics_df2_raw = pd.DataFrame(
-        {
-            "Receptor Pair": 
-            "KL Divergence": 
-            "EMD": 
-            "Data Type": "Raw Data"
-        }
+    print("EMD_vals", EMD_vals)
+    print("EMD_valsRAW", EMD_valsRAW)
+    print ("KL_div_vals", KL_div_vals)
+    print ("KL_div_valsRAW", KL_div_valsRAW)
+    metrics_raw = {
+        "Receptor Pair": receptors_of_interest,
+        "Data Type": ["Raw"] * len(receptors_of_interest),
+        "KL Divergence": KL_div_valsRAW,
+        "EMD": EMD_valsRAW
+    }
+    metrics_df2_raw = pd.DataFrame(metrics_raw)
+
+    # Prepare the abundance metrics DataFrame
+    metrics_abundance = {
+        "Receptor Pair": receptors_of_interest,
+        "Data Type": ["Abundance"] * len(receptors_of_interest),
+        "KL Divergence": KL_div_vals,
+        "EMD": EMD_vals
+    }
+    metrics_df2_abundance = pd.DataFrame(metrics_abundance)
+
+    # Melt the dataframes for Seaborn plotting
+    metrics_df2_raw_melted = metrics_df2_raw.melt(
+        id_vars=["Receptor Pair", "Data Type"],
+        value_vars=["KL Divergence", "EMD"],
+        var_name="Metric",
+        value_name="Value"
     )
 
-    metrics_df2_abundance = pd.DataFrame(
-        {
-            "Receptor Pair": 
-            "KL Divergence": 
-            "EMD": 
-            "Data Type": "Receptor Abundance"
-        }
+    metrics_df2_abundance_melted = metrics_df2_abundance.melt(
+        id_vars=["Receptor Pair", "Data Type"],
+        value_vars=["KL Divergence", "EMD"],
+        var_name="Metric",
+        value_name="Value"
     )
-    
-    metrics_df1_raw = pd.DataFrame(
-        {
-            "Receptor Pair": 
-            "KL Divergence": 
-            "EMD": 
-            "Data Type": "Raw Data"
-        }
-    )
-    
-    metrics_df1_abundance = pd.DataFrame(
-        {
-            "Receptor Pair": 
-            "KL Divergence": 
-            "EMD": 
-            "Data Type": "Receptor Abundance"
-        }
-    )
-    '''
-    print ("metrics_df2_raw", metrics_df2_raw)
-    print ("metrics_df2_abundance", metrics_df2_abundance)
-    #print ("metrics_df1_raw", metrics_df1_raw)
-    #print ("metrics_df1_raw", metrics_df1_raw)
-    sns.barplot(data=metrics_df2_raw.melt(id_vars=["Receptor Pair", "Data Type"], value_vars=["KL Divergence", "EMD"], var_name="Metric", value_name="Value"), 
-                x='Receptor Pair', y='Value', hue='Metric', ax=ax[0])
+
+    # Plotting
+    sns.barplot(data=metrics_df2_raw_melted, x='Receptor Pair', y='Value', hue='Metric', ax=ax[0])
     ax[0].set_title('KL Divergence and EMD for 2D Metrics (Raw Data)')
     ax[0].set_xticklabels(ax[0].get_xticklabels(), rotation=45, ha='right')
     ax[0].set_ylabel('Value')
 
-    sns.barplot(data=metrics_df2_abundance.melt(id_vars=["Receptor Pair", "Data Type"], value_vars=["KL Divergence", "EMD"], var_name="Metric", value_name="Value"), 
-                x='Receptor Pair', y='Value', hue='Metric', ax=ax[1])
+    sns.barplot(data=metrics_df2_abundance_melted, x='Receptor Pair', y='Value', hue='Metric', ax=ax[1])
     ax[1].set_title('KL Divergence and EMD for 2D Metrics (Receptor Abundance)')
     ax[1].set_xticklabels(ax[1].get_xticklabels(), rotation=45, ha='right')
     ax[1].set_ylabel('Value')
