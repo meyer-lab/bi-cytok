d0806ad1694512358a8e219b9f617627ece75b27
samuel-amidon <samidon@g.ucla.edu>
2025-11-07 11:22:20 -0800

    Fix scan indexing errors

--- bicytok/scanning_funcs.py
+++ bicytok/scanning_funcs.py
@@ -2,6 +2,7 @@
 Functions for calculating binding model selectivity, EMD, and KL divergence for many
 receptor combinations and target cell types.
 """
+import time
 
 import pandas as pd
 import numpy as np
@@ -25,11 +26,12 @@ def scan_KL_EMD(
     assert dim in [1, 2, 3]
     assert rec_abundances.shape[1] >= dim
 
-    output_shape = (rec_abundances.shape[0]) * dim + (len(targ_cell_types))
+    output_shape = (rec_abundances.shape[1],) * dim + (len(targ_cell_types),)
     EMD_vals_scan = np.full(output_shape, np.nan)
     KL_div_vals_scan = np.full(output_shape, np.nan)
 
     for i, cell_type in enumerate(targ_cell_types):
+        time_start = time.time()
         sampled_abun_DF = sample_receptor_abundances(
             pd.DataFrame(np.hstack((rec_abundances, np.array(cell_type_labels)[:, None])), columns=
                             [f"Rec_{j}" for j in range(rec_abundances.shape[1])] + ["Cell Type"]),
@@ -37,8 +39,8 @@ def scan_KL_EMD(
             targCellType=cell_type,
             balance=True
         )
-        sampled_cell_type_labels = sampled_abun_DF["Cell Type"].to_numpy()
-        sampled_rec_abundances = sampled_abun_DF.drop(columns=["Cell Type"]).to_numpy()
+        sampled_cell_type_labels = sampled_abun_DF["Cell Type"].to_numpy(dtype=str)
+        sampled_rec_abundances = sampled_abun_DF.drop(columns=["Cell Type"]).to_numpy(dtype=float)
 
         targ_mask = sampled_cell_type_labels == cell_type
         off_targ_mask = ~targ_mask
@@ -55,6 +57,7 @@ def scan_KL_EMD(
             KL_div_vals_scan[:, :, :, i], EMD_vals_scan[:, :, :, i] = KL_EMD_3D(
                 sampled_rec_abundances, targ_mask, off_targ_mask
             )
+        print(f"Completed KL/EMD scan for {cell_type} in {time.time() - time_start:.2f} seconds.")
 
     return KL_div_vals_scan, EMD_vals_scan
 
@@ -81,10 +84,11 @@ def scan_selectivity(
         assert len(valencies[0]) == dim
     assert signal_col is None or (0 <= signal_col < rec_abundances.shape[1])
 
-    output_shape = (rec_abundances.shape[0]) * dim + (len(targ_cell_types))
+    output_shape = (rec_abundances.shape[1],) * dim + (len(targ_cell_types),)
     selec_vals_scan = np.full(output_shape, np.nan)
-    opt_affs_scan = np.full(output_shape, np.nan)
     opt_Kx_star_scan = np.full(output_shape, np.nan)
+    affs_output_shape = (rec_abundances.shape[1],) * dim + (len(targ_cell_types), dim + (1 if signal_col is not None else 0))
+    opt_affs_scan = np.full(affs_output_shape, np.nan)
 
     for i, cell_type in enumerate(targ_cell_types):
         sampled_abun_DF = sample_receptor_abundances(
@@ -94,57 +98,58 @@ def scan_selectivity(
             targCellType=cell_type,
             balance=False # Balanced distributions not necessary for binding model (I think)
         )
-        sampled_cell_type_labels = sampled_abun_DF["Cell Type"].to_numpy()
-        sampled_rec_abundances = sampled_abun_DF.drop(columns=["Cell Type"]).to_numpy()
+        sampled_cell_type_labels = sampled_abun_DF["Cell Type"].to_numpy(dtype=str)
+        sampled_rec_abundances = sampled_abun_DF.drop(columns=["Cell Type"]).to_numpy(dtype=float)
 
         targ_mask = sampled_cell_type_labels == cell_type
-        print(sum(targ_mask))
         off_targ_mask = ~targ_mask
-        print(sum(off_targ_mask))
 
         if signal_col is not None:
             signal_rec_abun = np.reshape(sampled_rec_abundances[:, signal_col], (-1, 1))
 
+        opt_selecs = np.full((sampled_rec_abundances.shape[1],) * dim, np.nan)
+        if signal_col is not None:
+            opt_affs = np.full((sampled_rec_abundances.shape[1],) * dim + (dim + 1,), np.nan)
+        else:
+            opt_affs = np.full((sampled_rec_abundances.shape[1],) * dim + (dim,), np.nan)
+        opt_Kx_stars = np.full((sampled_rec_abundances.shape[1],) * dim, np.nan)
         if dim == 1:
-            opt_selecs = np.full((sampled_rec_abundances.shape[0]), np.nan)
-            if signal_col is not None:
-                opt_affs = np.full((sampled_rec_abundances.shape[0], dim + 1), np.nan)
-            else:
-                opt_affs = np.full((sampled_rec_abundances.shape[0], dim), np.nan)
-            opt_Kx_stars = np.full((sampled_rec_abundances.shape[0]), np.nan)
+            # opt_selecs = np.full((sampled_rec_abundances.shape[1]), np.nan)
+            # if signal_col is not None:
+            #     opt_affs = np.full((sampled_rec_abundances.shape[1], dim + 1), np.nan)
+            # else:
+            #     opt_affs = np.full((sampled_rec_abundances.shape[1], dim), np.nan)
+            # opt_Kx_stars = np.full((sampled_rec_abundances.shape[1]), np.nan)
 
             for j in range(sampled_rec_abundances.shape[1]):
                 rec_abun_pruned = np.reshape(sampled_rec_abundances[:, j], (-1, 1))
-                print(rec_abun_pruned.shape)
-                print(signal_rec_abun.shape)
 
                 if signal_col is not None:
                     rec_abun_pruned = np.hstack((signal_rec_abun, rec_abun_pruned))
-                    print(rec_abun_pruned.shape)
 
                 targ_recs = rec_abun_pruned[targ_mask, :]
-                print(targ_recs.shape)
                 off_targ_recs = rec_abun_pruned[off_targ_mask, :]
 
                 opt_selecs[j], opt_affs[j, :], opt_Kx_stars[j] = optimize_affs(
                     targ_recs, off_targ_recs, dose, valencies
                 )
+                opt_selecs[j] = 1 / opt_selecs[j]
 
             selec_vals_scan[:, i] = opt_selecs
-            # opt_affs_scan[:, i] = opt_affs
+            opt_affs_scan[:, i, :] = opt_affs
             opt_Kx_star_scan[:, i] = opt_Kx_stars
 
         if dim == 2:
-            opt_selecs = np.full((sampled_rec_abundances.shape[0]) * dim, np.nan)
-            opt_affs = np.full((sampled_rec_abundances.shape[0]) * dim, np.nan)
-            opt_Kx_stars = np.full((sampled_rec_abundances.shape[0]) * dim, np.nan)
+            # opt_selecs = np.full((sampled_rec_abundances.shape[0],) * dim, np.nan)
+            # opt_affs = np.full((sampled_rec_abundances.shape[0],) * dim, np.nan)
+            # opt_Kx_stars = np.full((sampled_rec_abundances.shape[0],) * dim, np.nan)
 
-            # Triangular indices assumes symmetry across the diagonal which is not
-            #   true if there is no designated signal recpetor or if valencies are
+            # Triangular indices assume symmetry across the diagonal which is not
+            #   true if there is no designated signal receptor or if valencies are
             #   asymmetric
             row, col = np.tril_indices(sampled_rec_abundances.shape[1], k=0)
             for rec1_ind, rec2_ind in zip(row, col, strict=False):
-                rec_abun_pruned = sampled_rec_abundances[:, rec1_ind, rec2_ind]
+                rec_abun_pruned = sampled_rec_abundances[:, [rec1_ind, rec2_ind]]
 
                 if signal_col is not None:
                     rec_abun_pruned = np.hstack((signal_rec_abun, rec_abun_pruned))
@@ -154,12 +159,13 @@ def scan_selectivity(
 
                 (
                     opt_selecs[rec1_ind, rec2_ind],
-                    opt_affs[rec1_ind, rec2_ind],
+                    opt_affs[rec1_ind, rec2_ind, :],
                     opt_Kx_stars[rec1_ind, rec2_ind],
                 ) = optimize_affs(targ_recs, off_targ_recs, dose, valencies)
+                opt_selecs[rec1_ind, rec2_ind] = 1 / opt_selecs[rec1_ind, rec2_ind]
 
             selec_vals_scan[:, :, i] = opt_selecs
-            opt_affs_scan[:, :, i] = opt_affs
+            opt_affs_scan[:, :, i, :] = opt_affs
             opt_Kx_star_scan[:, :, i] = opt_Kx_stars
 
         if dim == 3:
