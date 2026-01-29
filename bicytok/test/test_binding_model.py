b941dba2f6fbc0388d62029113e205a02a33cab3
samuel-amidon <samidon@g.ucla.edu>
2026-01-29 09:50:24 -0800

    Split init test by method

--- bicytok/test/test_binding_model.py
+++ bicytok/test/test_binding_model.py
@@ -86,13 +86,12 @@ def test_binding_model():
     assert R_bound.shape == recCounts.shape
 
 
-def test_starting_point_dependence():
+def test_rand_init():
     """
-    Test that the frequency of starting point-dependent failures for different
-    starting point methods is not too high.
-
-    Frequency thresholds are representative of the current state of the methods
-    and may be adjusted as methods improve/worsen.
+    Test that random initialization starting point method does not fail for too many receptor combinations.
+    
+    Frequency threshold is representative of the current state of the method
+    and may need to be adjusted as the method improves/worsens.
     """
     sample_size = 1000
     dose = 1e-10
@@ -102,15 +101,10 @@ def test_starting_point_dependence():
     ill_conditioned_recs = ["CD122", "CD338", "CD45RA"]
     test_recs = ["CD338", "CD45RA", "CD25", "CD4-1", "CD28", "CD278"]
     n_rands = 5
-    fixed_starting_point = [6.0, 7.0, 7.0, -9.0]
-
-    # Frequency thresholds
+    
     failed_selec_cutoff = 0.005
     random_init_fail_threshold = 0.6
-    init_search_fail_threshold = 0.1
-    fixed_start_fail_threshold = 0.05
-
-    # Import data
+    
     CITE_DF = importCITE()
     CITE_DF = CITE_DF.rename(columns={"CellType2": "Cell Type"})
     CITE_DF = CITE_DF.drop(columns="CellType1")
@@ -158,10 +152,51 @@ def test_starting_point_dependence():
     rand_fail_freq = sum(
         selec <= low_selec + failed_selec_cutoff for selec in random_selec_list
     ) / len(random_selec_list)
+    print(f"Random initialization failure frequency: {rand_fail_freq}")
     assert rand_fail_freq < random_init_fail_threshold
 
-    # Test initialization search
+
+def test_search_init():
+    """
+    Test that initialization search starting point method does not fail for too many receptor combinations.
+    """
+    sample_size = 1000
+    dose = 1e-10
+    valencies = np.array([[2, 1, 1]])
+    cell_type = "Treg"
+    targ_rec = "CD122"
+    ill_conditioned_recs = ["CD122", "CD338", "CD45RA"]
+    test_recs = ["CD338", "CD45RA", "CD25", "CD4-1", "CD28", "CD278"]
+    
+    failed_selec_cutoff = 0.005
+    init_search_fail_threshold = 0.1
+    
+    CITE_DF = importCITE()
+    CITE_DF = CITE_DF.rename(columns={"CellType2": "Cell Type"})
+    CITE_DF = CITE_DF.drop(columns="CellType1")
+    CITE_DF = CITE_DF.drop(columns="CellType3")
+    sample_DF = sample_receptor_abundances(
+        CITE_DF=CITE_DF,
+        numCells=sample_size,
+        targCellType=cell_type,
+    )
+    targ_mask = (sample_DF["Cell Type"] == cell_type).to_numpy()
+    
+    init_params = [12, 12, 12, -15]
+    low_selec = (
+        1
+        / optimize_affs(
+            targRecs=sample_DF[ill_conditioned_recs].to_numpy()[targ_mask],
+            offTargRecs=sample_DF[ill_conditioned_recs].to_numpy()[~targ_mask],
+            dose=dose,
+            valencies=valencies,
+            init_vals=init_params,
+        )[0]
+    )
+    print(f"Ill-conditioned optimization yields selectivity: {low_selec}")
+    
     init_search_selec_list = []
+    row, col = np.tril_indices(len(test_recs), k=0)
     for i, j in zip(row, col, strict=False):
         rec1 = test_recs[i]
         rec2 = test_recs[j]
@@ -180,10 +215,52 @@ def test_starting_point_dependence():
     search_fail_freq = sum(
         selec <= low_selec + failed_selec_cutoff for selec in init_search_selec_list
     ) / len(init_search_selec_list)
+    print(f"Initialization search failure frequency: {search_fail_freq}")
     assert search_fail_freq < init_search_fail_threshold
 
-    # Test fixed starting point
+
+def test_fixed_init():
+    """
+    Test that fixed starting point method does not fail for too many receptor combinations.
+    """
+    sample_size = 1000
+    dose = 1e-10
+    valencies = np.array([[2, 1, 1]])
+    cell_type = "Treg"
+    targ_rec = "CD122"
+    ill_conditioned_recs = ["CD122", "CD338", "CD45RA"]
+    test_recs = ["CD338", "CD45RA", "CD25", "CD4-1", "CD28", "CD278"]
+    fixed_starting_point = [6.0, 7.0, 7.0, -9.0]
+    
+    failed_selec_cutoff = 0.005
+    fixed_start_fail_threshold = 0.05
+    
+    CITE_DF = importCITE()
+    CITE_DF = CITE_DF.rename(columns={"CellType2": "Cell Type"})
+    CITE_DF = CITE_DF.drop(columns="CellType1")
+    CITE_DF = CITE_DF.drop(columns="CellType3")
+    sample_DF = sample_receptor_abundances(
+        CITE_DF=CITE_DF,
+        numCells=sample_size,
+        targCellType=cell_type,
+    )
+    targ_mask = (sample_DF["Cell Type"] == cell_type).to_numpy()
+    
+    init_params = [12, 12, 12, -15]
+    low_selec = (
+        1
+        / optimize_affs(
+            targRecs=sample_DF[ill_conditioned_recs].to_numpy()[targ_mask],
+            offTargRecs=sample_DF[ill_conditioned_recs].to_numpy()[~targ_mask],
+            dose=dose,
+            valencies=valencies,
+            init_vals=init_params,
+        )[0]
+    )
+    print(f"Ill-conditioned optimization yields selectivity: {low_selec}")
+    
     fixed_selec_list = []
+    row, col = np.tril_indices(len(test_recs), k=0)
     for i, j in zip(row, col, strict=False):
         rec1 = test_recs[i]
         rec2 = test_recs[j]
@@ -202,6 +279,7 @@ def test_starting_point_dependence():
     fixed_fail_freq = sum(
         selec <= low_selec + failed_selec_cutoff for selec in fixed_selec_list
     ) / len(fixed_selec_list)
+    print(f"Fixed starting point failure frequency: {fixed_fail_freq}")
     assert fixed_fail_freq < fixed_start_fail_threshold
 
 
