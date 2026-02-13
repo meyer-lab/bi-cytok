68544bc0bbb834b4ea65aa6044b47e0320a46b29
samuel-amidon <samidon@g.ucla.edu>
2026-02-12 16:03:08 -0800

    Clean up unused imports

--- bicytok/imports.py
+++ bicytok/imports.py
@@ -1,4 +1,4 @@
-"""File that deals with everything about importing and sampling."""
+"""File for importing and sampling CITE-seq data."""
 
 from pathlib import Path
 from zipfile import ZipFile
@@ -8,108 +8,18 @@ import pandas as pd
 
 path_here = Path(__file__).parent.parent
 
-SC_Stims = [
-    "control",
-    "IL2_100pM",
-    "IL2_1nM",
-    "IL2_10nM",
-    "IL2_50nM",
-    "IL2_200nM",
-    "IL7_100nM",
-    "IL10_500nM",
-    "IL10_2000nM",
-    "TGFB_10nM",
-    "TGFB_50nM",
-]  # "IL7_500nM is blank"
-
-
-# Originally called in selectivityFuncs.getConvFactDict
-def getBindDict():
-    """Gets binding to pSTAT fluorescent conversion dictionary"""
-    bindingDF = pd.read_csv(
-        path_here / "bicytok" / "data" / "BindingConvDict.csv", encoding="latin1"
-    )
-    return bindingDF
-
-
-# Sam: Not called anywhere, not sure what original use was
-def importReceptors():
-    """Makes Complete receptor expression Dict"""
-    recDF = pd.read_csv(path_here / "bicytok" / "data" / "RecQuantitation.csv")
-    recDFbin = pd.read_csv(path_here / "bicytok" / "data" / "BinnedReceptorData.csv")
-    recDFbin = recDFbin.loc[recDFbin["Bin"].isin([1, 3])]
-    recDFbin.loc[recDFbin["Bin"] == 1, "Cell Type"] += r" $IL2Ra^{lo}$"
-    recDFbin.loc[recDFbin["Bin"] == 3, "Cell Type"] += r" $IL2Ra^{hi}$"
-    recDF = pd.concat([recDF, recDFbin])
-    return recDF
-
-
-# Not called anywhere
-def makeCITEdf():
-    """Makes cite surface epitope csv for given cell type,
-    DON'T USE THIS UNLESS DATA NEEDS RESTRUCTURING"""
-    """
-    matrixDF = pd.read_csv(join(path_here, "bicytok/data/CITEmatrix.gz"),
-        compression='gzip', header=0, sep=' ', quotechar='"', error_bad_lines=False)
-    matrixDF = matrixDF.iloc[:, 0:-2]
-    matrixDF.columns = ["Marker", "Cell", "Number"]
-    matrixDF.to_csv(join(path_here, "bicytok/data/CITEmatrix.csv"), index=False)
-    """
-    featureDF = pd.read_csv(path_here / "bicytok" / "data" / "CITEfeatures.csv")
-    matrixDF = pd.read_csv(path_here / "bicytok" / "data" / "CITEmatrix.csv").iloc[
-        1::, :
-    ]
-    metaDF = pd.read_csv(path_here / "bicytok" / "data" / "metaData3P.csv")
-
-    metaDF["cellNumber"] = metaDF.index + 1
-    cellNums = metaDF.cellNumber.values
-    cellT1 = metaDF["celltype.l1"].values
-    cellT2 = metaDF["celltype.l2"].values
-    cellT3 = metaDF["celltype.l3"].values
-    cellTDict1 = {cellNums[i]: cellT1[i] for i in range(len(cellNums))}
-    cellTDict2 = {cellNums[i]: cellT2[i] for i in range(len(cellNums))}
-    cellTDict3 = {cellNums[i]: cellT3[i] for i in range(len(cellNums))}
-
-    featureDF["featNumber"] = featureDF.index + 1
-    featNums = featureDF.featNumber.values
-    features = featureDF.Marker.values
-    featDict = {featNums[i]: features[i] for i in range(len(featNums))}
-    matrixDF["Marker"] = matrixDF["Marker"].replace(featDict)
-
-    categories1 = metaDF["celltype.l1"].unique()
-    categories2 = metaDF["celltype.l2"].unique()
-    categories3 = metaDF["celltype.l3"].unique()
-
-    matrixDF = (
-        matrixDF.pivot(index=["Cell"], columns="Marker", values="Number")
-        .reset_index()
-        .fillna(0)
-    )
-
-    matrixDF["CellType1"] = pd.Categorical(
-        matrixDF["Cell"].replace(cellTDict1), categories=categories1
-    )
-    matrixDF["CellType2"] = pd.Categorical(
-        matrixDF["Cell"].replace(cellTDict2), categories=categories2
-    )
-    matrixDF["CellType3"] = pd.Categorical(
-        matrixDF["Cell"].replace(cellTDict3), categories=categories3
-    )
-    matrixDF.to_csv(path_here / "bicytok" / "data" / "CITEdata.csv", index=False)
-    return matrixDF  # , featureDF, metaDF
-
 
 def importCITE():
     """Downloads all surface markers and cell types"""
     CITEmarkerDF = pd.read_csv(
-        path_here / "bicytok" / "data" / "CITEdata_SurfMarkers.zip"
+        path_here / "data" / "CITEdata_SurfMarkers.zip"
     )
     return CITEmarkerDF
 
 
 def importRNACITE():
     """Downloads all surface markers and cell types"""
-    with ZipFile(path_here / "bicytok" / "data" / "RNAseqSurface.csv.zip") as zip_file:
+    with ZipFile(path_here / "data" / "RNAseqSurface.csv.zip") as zip_file:
         RNAsurfDF = pd.read_csv(zip_file.open("RNAseqSurface.csv"))
     return RNAsurfDF
 
