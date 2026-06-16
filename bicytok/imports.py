fade47f315711ab1cd2091680d95370cfcd3e0e3
samuel-amidon <samidon@g.ucla.edu>
2026-06-16 12:10:30 -0700

    Suppress KDE singularity warning

--- bicytok/imports.py
+++ bicytok/imports.py
@@ -72,9 +72,7 @@ def sample_receptor_abundances(
     # Sample an equal number of target and off-target cells
     target_cells = CITE_DF[CITE_DF["Cell Type"] == targCellType]
     if offTargCellTypes is not None:
-        off_target_cells = CITE_DF[
-            CITE_DF["Cell Type"].isin(offTargCellTypes)
-        ]
+        off_target_cells = CITE_DF[CITE_DF["Cell Type"].isin(offTargCellTypes)]
     else:
         off_target_cells = CITE_DF[CITE_DF["Cell Type"] != targCellType]
 
