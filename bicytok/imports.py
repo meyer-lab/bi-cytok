a1909bd23def6eb508bc444ff0e5544d4af59661
samuel-amidon <samidon@g.ucla.edu>
2026-06-24 16:08:52 -0700

    Add RNA-annotated import option

--- bicytok/imports.py
+++ bicytok/imports.py
@@ -9,9 +9,15 @@ import pandas as pd
 path_here = Path(__file__).parent.parent
 
 
-def importCITE():
+def importCITE(type: str = ""):
     """Downloads all surface markers and cell types"""
-    CITEmarkerDF = pd.read_csv(path_here / "data" / "CITEdata_SurfMarkers.zip")
+    if type == "RNA_annotated":
+        CITEmarkerDF = pd.read_csv(path_here / "data" / "CITEdata_SurfMarkers_RNA_annotated.csv.zip")
+        CITEmarkerDF["CellType1"] = CITEmarkerDF["CellType1_RNA"]
+        CITEmarkerDF["CellType2"] = CITEmarkerDF["CellType2_RNA"]
+        CITEmarkerDF = CITEmarkerDF.drop(columns=["CellType1_RNA", "CellType2_RNA"])
+    else:
+        CITEmarkerDF = pd.read_csv(path_here / "data" / "CITEdata_SurfMarkers.zip")
     return CITEmarkerDF
 
 
